aaf4bdcb2aec927764183d405873e0284358df7b
vishnuraj-m <acappella.studiov@gmail.com>
2022-09-14 15:22:14 +0530

    home page ui completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
-
+// routing
+import { Route, Routes } from 'react-router-dom';
 // React Slick
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+// pages
+import HomePage from './pages/HomePage';
+import MoviePage from './pages/MoviePage';
+import PlayPage from './pages/PlayPage';
 
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Routes>
+      <Route path='/' element={<HomePage/>}/>
+      <Route path='/movie/:id' element={<MoviePage/>}/>
+      <Route path='/plays' element={<PlayPage/>}/>
+    </Routes>
   );
 }
 
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { useState } from 'react';
+// component
+import HeroCarousel from '../components/hero_carousel/HeroCarousel';
+import EntertainmentCard from '../components/entertainment/EntertainmentCard';
+import PosterSlider from '../components/poster_slider/PosterSlider';
+// layoutHoc
+import DefaultLayoutHoc from '../layouts/DefaultLayout';
+
+
+const HomePage = () => {
+  const [recommendedMoves, setRecommendedMoves] = useState([]);
+  const [premierMoves, setPremierMoves] = useState([]);
+  const [onlineStreamEvents, setOnlineStreamEvents] = useState([]);
+  return (
+    <>
+      <HeroCarousel />
+      <div className='container mx-auto px-4 md:px-12 my-8'>
+        <h1 className='text-2xl font-bold text-gray-800 sm:ml-3 ml-0 my-3'>The best of Entertainment</h1>
+        <EntertainmentCard />
+      </div>
+
+      <div className='container mx-auto px-4 md:px-12 my-8'>
+        <PosterSlider
+          title='Recommended Movies'
+          subject='List of Recommended Movies'
+          posters={recommendedMoves}
+          isDark={false} />
+      </div>
+
+      <div className='bg-premier-800'>
+        <div className='container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3'>
+          <div className='hidden md:flex'>
+            <img src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png"
+              alt="Rupay"
+              className='h-full w-full' />
+          </div>
+          <PosterSlider
+            title='Premier Movies'
+            subject='Brand new releases every Friday'
+            posters={premierMoves}
+            isDark={true} />
+        </div>
+      </div>
+      <div className='container mx-auto px-4 md:px-12 my-8'>
+        <PosterSlider
+          title='Online Streaming Events'
+          subject=''
+          posters={onlineStreamEvents}
+          isDark={false} />
+      </div>
+    </>
+  );
+};
+
+export default DefaultLayoutHoc(HomePage);
