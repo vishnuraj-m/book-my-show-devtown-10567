909532fd37003f5b87fd0cf8db010960de33e49c
vishnuraj-m <acappella.studiov@gmail.com>
2022-09-17 21:37:16 +0530

    home page completed

--- src/components/poster_slider/PosterSlider.jsx
+++ src/components/poster_slider/PosterSlider.jsx
@@ -3,8 +3,7 @@ import Slider from 'react-slick';
 import Poster from '../poster/Poster';
 
 const PosterSlider = (props) => {
-
-  const { title, subtitle, posters, isDark } = props;
+  const { posters, title, subtitle, isDark, config } = props;
 
   const settings = {
     infinite: false,
@@ -35,21 +34,38 @@ const PosterSlider = (props) => {
         },
       },
     ],
-  }
+  };
 
   return (
     <>
-      <div className=' flex flex-col items-start sm:ml-3 ml-0 my-2'>
-        <h3 className={`text-2xl font-bold ${isDark ? "text-white" : "text-black"}`}>{title}</h3>
-        <p className={`text-sm ${isDark ? "text-white" : "text-gray-800"}`}>{subtitle}</p>
+      <div className="flex flex-col items-start sm:ml-3 ml-0 my-2">
+        <h3
+          className={`text-2xl font-bold ${
+            isDark ? "text-white" : "text-black"
+          }`}
+        >
+          {title}
+        </h3>
+        <p className={`text-sm ${isDark ? "text-white" : "text-gray-800"}`}>
+          {subtitle}
+        </p>
       </div>
-      <Slider {...settings}>
-        {posters.map((each, index) => (
-          <Poster {...each} isDark={isDark} key={index} />
-        ))}
-      </Slider>
+      {config && (
+        <Slider {...config}>
+          {posters.map((each, index) => (
+            <Poster {...each} isDark={isDark} key={index} />
+          ))}
+        </Slider>
+      )}
+      {!config && (
+        <Slider {...settings}>
+          {posters.map((each, index) => (
+            <Poster {...each} isDark={isDark} key={index} />
+          ))}
+        </Slider>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default PosterSlider
+export default PosterSlider;
