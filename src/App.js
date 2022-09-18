29e31051036e91bff1235bee6c21aa5549bbe6c9
vishnuraj-m <acappella.studiov@gmail.com>
2022-09-18 17:42:51 +0530

    env config

--- src/App.js
+++ src/App.js
@@ -10,10 +10,9 @@ import MoviePage from './pages/MoviePage';
 import PlayPage from './pages/PlayPage';
 import axios from 'axios';
 
-axios.defaults.baseURL="https://api.themoviedb.org/3"
-axios.defaults.params ={};
-axios.defaults.params["app_key"]= process.env.REACT_APP_API_KEY;
-
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+axios.defaults.params = {};
+axios.defaults.params["api_key"] = process.env.REACT_APP_API_KEY;
 
 function App() {
   return (
