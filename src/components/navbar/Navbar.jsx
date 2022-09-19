d595d10c373db66f557618d3d165199610431688
vishnuraj-m <acappella.studiov@gmail.com>
2022-09-19 16:38:19 +0530

    navbar section completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,10 +9,10 @@ const Navbar = () => {
         <div className='text-white flex items-center justify-between'>
           <div>
             <h3 className='text-xl font-bold'>It All Starts Here!</h3>
-            <span className='text-gray-400 text-xs flex items-center cursor-pointer hover:text-white'>Dlehi NCR <BiChevronDown/></span>
+            <span className='text-gray-400 text-xs flex items-center cursor-pointer hover:text-white'>Dlehi NCR <BiChevronDown /></span>
           </div>
           <div className='w-7 h-7'>
-            <BiSearch className='w-full h-full'/>
+            <BiSearch className='w-full h-full' />
           </div>
         </div>
       </>
@@ -22,24 +22,24 @@ const Navbar = () => {
   function NavMd() {
     return (
       <>
-      <div className='flex items-center w-full gap-3'>
+        <div className='flex items-center w-full gap-3'>
 
-        <div className='w-10 h-10'>
-          <img src="https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png" 
-          alt="logo" 
-          className='w-full h-full'
-          />
+          <div className='w-10 h-10'>
+            <img src="https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png"
+              alt="logo"
+              className='w-full h-full'
+            />
+          </div>
+
+          <div className='w-full flex items-center gap-3 bg-white px-3 py-1 rounded-md'>
+            <BiSearch />
+            <input type="search"
+              className='w-full bg-transparent border-none focus:outline-none'
+              placeholder='Search for movies, events, plays, sports and activities'
+            />
+          </div>
+
         </div>
-
-        <div className='w-full flex items-center gap-3 bg-white px-3 py-1 rounded-md'>
-          <BiSearch/>
-          <input type="search" 
-          className='w-full bg-transparent border-none focus:outline-none'
-          placeholder='Search for movies, events, plays, sports and activities'
-          />
-        </div>
-
-      </div>
       </>
     );
   };
@@ -47,35 +47,35 @@ const Navbar = () => {
   function NavLg() {
     return (
       <>
-      <div className='container flex mx-auto px-4 items-center justify-between'>
-        <div className='flex items-center w-1/2 gap-3'>
-          <div className='w-10 h-10'>
-            <img src="https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png" 
-            alt="Logo" 
-            className='w-full h-full'
-            />
+        <div className='container flex mx-auto px-4 items-center justify-between'>
+          <div className='flex items-center w-1/2 gap-3'>
+            <div className='w-10 h-10'>
+              <img src="https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png"
+                alt="Logo"
+                className='w-full h-full'
+              />
+            </div>
+            <div className='w-full flex items-center gap-3 bg-white px-3 py-1 rounded-md'>
+              <BiSearch />
+              <input
+                type="search"
+                className='w-full bg-transparent border-none focus:outline-none'
+                placeholder='Search for movies, events, plays, sports and activities'
+              />
+            </div>
           </div>
-          <div className='w-full flex items-center gap-3 bg-white px-3 py-1 rounded-md'>
-            <BiSearch/>
-            <input 
-            type="search" 
-            className='w-full bg-transparent border-none focus:outline-none'
-            placeholder='Search for movies, events, plays, sports and activities'
-            />
+          <div className='flex items-center gap-3'>
+            <span className='text-gray-200 text-base flex items-center cursor-pointer hover:text-white'>
+              Delhi NCR <BiChevronDown />
+            </span>
+            <button className='bg-red-600 text-white px-2 py-1 text sm rounded'>
+              Sign In
+            </button>
+            <div className='w-8 h-8 text-white'>
+              <BiMenu className='w-full h-full' />
+            </div>
           </div>
         </div>
-        <div className='flex items-center gap-3'>
-          <span className='text-gray-200 text-base flex items-center cursor-pointer hover:text-white'>
-            Delhi NCR <BiChevronDown/>
-          </span>
-          <button className='bg-red-600 text-white px-2 py-1 text sm rounded'>
-            Sign In
-          </button>
-          <div className='w-8 h-8 text-white'>
-            <BiMenu className='w-full h-full'/>
-          </div>
-        </div>
-      </div>
       </>
     );
   };
@@ -85,17 +85,17 @@ const Navbar = () => {
 
       {/* Mobile Screen NavBar */}
       <div className='md:hidden'>
-        <NavSm/>
+        <NavSm />
       </div>
 
       {/* Medium/Tab Screen NavBar */}
       <div className='hidden md:flex lg:hidden'>
-        <NavMd/>
+        <NavMd />
       </div>
 
       {/* Large Screen NavBar */}
       <div className='hidden md:hidden lg:flex'>
-        <NavLg/>
+        <NavLg />
       </div>
     </nav>
   )
